bd757177f03a1e5964d67121ce2f0f30f74da729
Michael Li <michael.li11702@gmail.com>
2016-02-13 02:25:31 -0800

    Write function to output a line, other cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,13 @@
 #include <string>
 #include <vector>
 #include <random>
-#include <cstdarg>
 
 using namespace std;
 
 /*
-Random distribution objects are lightweight, so we simply construct a new
-distribution object every time we need a random number
-*/
+ * Random distribution objects are lightweight, so we simply construct a new
+ * distribution object every time we need a random number
+ */
 class Random {
 public:
     Random() = default;
@@ -25,31 +24,46 @@ int Random::drawNumber(int min, int max) {
     return uniform_int_distribution<int>{min, max}(eng);
 }
 
+/*
+ * A counter of how many tabs to output at the start of the line;
+ * increment it whenever we reach a new scope
+ */
+unsigned g_currentTabCount = 0;
+
+void coutLine(const string lineStr = "") {
+	for (unsigned i = 0; i < g_currentTabCount; i++) {
+		cout << '\t';
+	}
+	cout << lineStr << '\n';
+}
+
 void generateHeader() {
 	vector<string> stdLibraries = {"stdio.h", "iostream", "string", "stdlib.h", "math.h"};
 	for (unsigned i = 0; i < stdLibraries.size(); i++) {
-		cout <<  "#include <" << stdLibraries[i] << ">\n";	
+		coutLine("#include <" + stdLibraries[i] + ">");
 	}
-	cout << "\n";
-	cout << "using namespace std;\n\n";
+	coutLine();
+	coutLine("using namespace std;");
+	coutLine();
 }
 
 void generateMainFunction() {
-	cout << "int main() {\n";
-	// use endl here instead of \n to make the cout more clear	
-	cout << "\tcout << \"Hello world!\" << '\\n';" << '\n';
-	cout << "\treturn 0;\n";
-	cout << "}\n";
+	coutLine("int main() {");
+	g_currentTabCount++;
+	coutLine("cout << \"Hello world!\" << '\\n';");
+	coutLine("return 0;");
+	g_currentTabCount--;
+	coutLine("}");
 }
 
 int main(int argc, char** argv) {
 	if (argc < 2) {
-		cout << "Need a filename!\n";
+		cerr << "Need a filename!\n";
 		return 1;
 	}
 	char *filename = argv[1];
 	if (!freopen(filename, "w", stdout)) {
-		cout << filename << " could not be opened for writing\n";
+		cerr << filename << " could not be opened for writing\n";
 		return 1;
 	}
 	generateHeader();
