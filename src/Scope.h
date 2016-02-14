b88f3365a300b103113a31f0a251cb5518e3fd92
Richard Sun <rsun@media.ucla.edu>
2016-02-14 00:49:55 -0800

    comments for Scope interface

--- src/Scope.h
+++ src/Scope.h
@@ -15,8 +15,14 @@ class Scope {
 		void addVar(string type, string name);
 
 	public:
+		/* Returns the name of a new variable */
 		string newVar(string type);
+
+		/* Return a new variable name, and try to name the variable the
+		 * given name */
 		string newVar(string type, string name);
+
+		/* Return a random variable name of the given type */
 		string getVar(string type);
 };
 
