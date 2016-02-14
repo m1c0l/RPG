84cd84fc74dac02d8e55559092f82055e8220ebf
Michael Li <michael.li11702@gmail.com>
2016-02-14 00:59:35 -0800

    Moved Random code to its own files

---
+++ src/Random.h
@@ -0,0 +1,29 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <random>
+
+using namespace std;
+
+/*
+ * Use long long for random number generator as this is the biggest data type;
+ * this just means we can't generate unsigned long longs without some casting,
+ * though it should work since signed to unsigned conversion is defined.
+ */
+typedef long long random_t;
+/*
+ * Random distribution objects are lightweight, so we simply construct a new
+ * distribution object every time we need a random number
+ */
+class Random {
+public:
+	Random() = default;
+	Random(mt19937::result_type seed) : eng(seed) {}
+	random_t drawNumber(random_t min, random_t max);
+
+private:        
+    mt19937_64 eng{random_device{}()};
+};
+
+#endif
+
--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,10 @@
 #include <random>
 #include <limits.h>
 
+#include "Random.h"
+#include "Scope.h"
+
 using namespace std;
-
-/*
- * Use long long for random number generator as this is the biggest data type;
- * this just means we can't generate unsigned long longs without some casting,
- * though it should work since signed to unsigned conversion is defined.
- */
-typedef long long random_t;
-/*
- * Random distribution objects are lightweight, so we simply construct a new
- * distribution object every time we need a random number
- */
-class Random {
-public:
-	Random() = default;
-	Random(mt19937::result_type seed) : eng(seed) {}
-	random_t drawNumber(random_t min, random_t max);
-
-private:        
-    mt19937_64 eng{random_device{}()};
-};
-
-random_t Random::drawNumber(random_t min, random_t max) {
-	return uniform_int_distribution<random_t>{min, max}(eng);
-}
 
 /*
  * A counter of how many tabs to output at the start of the line;
