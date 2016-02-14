2d594f15f305f7346bc8af2624ad7e5114dd809d
Michael Li <michael.li11702@gmail.com>
2016-02-13 16:19:13 -0800

    Make random number generator more robust, output random numbers in generated program

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,32 @@
 #include <string>
 #include <vector>
 #include <random>
+#include <limits.h>
 
 using namespace std;
 
+/*
+ * Use long long for random number generator as this is the biggest data type;
+ * this just means we can't generate unsigned long longs without some casting,
+ * though it should work since signed to unsigned conversion is defined.
+ */
+typedef long long random_t;
 /*
  * Random distribution objects are lightweight, so we simply construct a new
  * distribution object every time we need a random number
  */
 class Random {
 public:
-    Random() = default;
-    Random(mt19937::result_type seed) : eng(seed) {}
-    int drawNumber(int min, int max);
+	Random() = default;
+	Random(mt19937::result_type seed) : eng(seed) {}
+	random_t drawNumber(random_t min, random_t max);
 
 private:        
-    mt19937 eng{random_device{}()};
+    mt19937_64 eng{random_device{}()};
 };
 
-int Random::drawNumber(int min, int max) {
-    return uniform_int_distribution<int>{min, max}(eng);
+random_t Random::drawNumber(random_t min, random_t max) {
+	return uniform_int_distribution<random_t>{min, max}(eng);
 }
 
 /*
@@ -37,6 +44,7 @@ void coutLine(const string lineStr = "") {
 	cout << lineStr << '\n';
 }
 
+
 void generateHeader() {
 	vector<string> stdLibraries = {"stdio.h", "iostream", "string", "stdlib.h", "math.h"};
 	for (unsigned i = 0; i < stdLibraries.size(); i++) {
@@ -51,6 +59,11 @@ void generateMainFunction() {
 	coutLine("int main() {");
 	g_currentTabCount++;
 	coutLine("cout << \"Hello world!\" << '\\n';");
+	Random randGen;
+	random_t tmpRand = randGen.drawNumber(LLONG_MIN, LLONG_MAX);
+	coutLine("long long ll = " + to_string(tmpRand) + ";");
+	tmpRand = randGen.drawNumber(CHAR_MIN, CHAR_MAX);
+	coutLine("char c = " + to_string(tmpRand) + ";");
 	coutLine("return 0;");
 	g_currentTabCount--;
 	coutLine("}");
