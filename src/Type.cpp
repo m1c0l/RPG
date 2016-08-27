7bae2f28ea957095ffbf19f456e8f274319a27ab
Michael Li <michael.li11702@gmail.com>
2016-08-27 14:39:39 -0400

    Put type mins/maxes into an array

--- src/Type.cpp
+++ src/Type.cpp
@@ -1,8 +1,6 @@
 #include "Type.h"
 #include "Random.h"
 
-#include <limits.h>
-
 using namespace std;
 
 SupportedType getRandType() {
@@ -11,24 +9,6 @@ SupportedType getRandType() {
 
 string getRandValue(SupportedType type) {
 	string randString;
-	random_t randLong;
-	switch(type) {
-	case CHAR:
-		randLong = g_randGen.drawNumber(CHAR_MIN, CHAR_MAX);
-		randString = to_string(randLong);
-		break;
-	case INT:
-		randLong = g_randGen.drawNumber(INT_MIN, INT_MAX);
-		randString = to_string(randLong);
-		break;
-	case LONG:
-		randLong = g_randGen.drawNumber(LONG_MIN, LONG_MAX);
-		randString = to_string(randLong);
-		break;
-	case LLONG:
-		randLong = g_randGen.drawNumber(LLONG_MIN, LLONG_MAX);
-		randString = to_string(randLong);
-		break;
-	}
-	return randString;
+	random_t randLong = g_randGen.drawNumber(g_typeMinValues[type], g_typeMaxValues[type]);
+	return to_string(randLong);
 }
--- src/Type.h
+++ src/Type.h
@@ -4,11 +4,16 @@
 #include <string>
 #include <unordered_map>
 
+#include <limits.h>
+
 using namespace std;
 
 enum SupportedType { CHAR = 0, INT, LONG, LLONG };
 /* As long as the orders match, we can easily access an enum value's corresponding string */
 const string g_typeStrings[] = {"char", "int", "long", "long long" };
+
+const long long g_typeMinValues[] = { CHAR_MIN, INT_MIN, LONG_MIN, LLONG_MIN };
+const long long g_typeMaxValues[] = { CHAR_MAX, INT_MAX, LONG_MAX, LLONG_MAX };
 
 #define NUM_SUPPORTED_TYPES 4
 
